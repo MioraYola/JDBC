4e8e24470e589ff019bbae862d960ef75347ccb7
yola <miorayola07@gmail.com>
2025-12-01 17:04:26 +0100

    connect db

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Main {
+    private static final String URL= "jdbc:postgresql://localhost:5432/testjdbc" ;
+    private static final String USER = "postgres";
+    private static final String PASSWORD= "raketa"; 
+    
+
+}
