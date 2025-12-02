d051c63582c31f65e7e66239937683861fc9a5e6
yola <miorayola07@gmail.com>
2025-12-02 10:33:53 +0100

    connect the db

---
+++ src/main/java/com/example/CreateUser.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.sql.Connection;
+
+public class CreateUser {
+    public static void create(Connection conn, String nom, String prenom){
+        try (){
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,16 @@
 package com.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     private static final String URL= "jdbc:postgresql://localhost:5432/testjdbc" ;
     private static final String USER = "postgres";
     private static final String PASSWORD= "raketa"; 
-    
+
+    public static Connection gConnection() throws SQLException{
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 
 }
